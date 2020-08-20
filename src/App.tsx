d64203ed2e713f028e72cd96374e12d5dd7a50f2
Viniciusguedes <viniciusguedes@lavid.ufpb.br>
2020-08-20 00:12:02 -0300

    api connection added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
-const App: React.FC = () => <h1>Hello World</h1>;
+interface Application {
+  name: string;
+}
+
+const App: React.FC = () => {
+  const [applications, setApplications] = useState<Application[]>([]);
+
+  useEffect(() => {
+    api.get('/applications').then(response => {
+      return setApplications(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>hello world</h1>
+      {applications &&
+        applications.map(application => <h1>{application.name}</h1>)}
+    </>
+  );
+};
 
 export default App;
